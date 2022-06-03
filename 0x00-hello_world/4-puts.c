197047bad311658c47fd82ca862feb8a720864f1
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-06-02 18:20:55 -0700

    put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+
+int main () 
+{
+	char str1[40];
+	strcpy(str1, "Programming is like building a multilingual puzzle");
+	puts(str1);
+	return (o);
+}	
