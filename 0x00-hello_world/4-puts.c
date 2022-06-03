f8d6535967368ec05d446f33bdf546d12bb24380
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-06-02 18:36:13 -0700

    put funct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,27 @@
-#!/bin/bash
 #include <stdio.h>
-#include <string.h>
 
-int main () 
+/**
+ *
+ *  * main - return the integal of the function
+ *
+ *
+ *    * Description:use to print "programming is like building a mutilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: value of 0
+ 
+ *
+ *        */
+
+
+
+int main(void)
+
 {
-	char str1[40];
-	strcpy(str1, "Programming is like building a multilingual puzzle");
-	puts(str1);
-	return (o);
-}	
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
