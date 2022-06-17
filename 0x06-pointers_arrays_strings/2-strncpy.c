e6560784485f56cb934882736a95089ce4eae8ce
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-06-16 20:37:23 -0700

    Task 2 and 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
-  /**
-  * _strncpy - copy a string from index of 0 of 'dest'
-  * @dest: first argument
-  * @src: second argument
-  * @n: third aargument
-  * Description: return the required result
-  * Return: return char pointer
-  */
+ /**
+ * _strncpy - copy a string from index of 0 of 'dest'
+ * @src: string
+ * Description: return the required result
+ * Return: return char pointer
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
