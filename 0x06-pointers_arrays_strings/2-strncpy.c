68d30eaa57c2b7d59b8b0f07a77169936cd74b22
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-06-22 07:54:32 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
- /**
- * _strncpy - copy a string from index of 0 of 'dest'
- * @src: string
- * Description: return the required result
- * Return: return char pointer
- */
+/**
+  * _strncpy - copy a string from index of 0 of 'dest'
+  * @src: string
+  * Description: return the required result
+  * Return: return char pointer
+  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
 
 	for (; n > i; i++)
-		dest[i] = '\0';
+		*(dest + i) = '\0';
 
 	return (dest);
 }
