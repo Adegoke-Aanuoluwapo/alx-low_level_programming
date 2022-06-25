1d0f0c0203014ee30066c22d1ca3bf60c118bd37
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-06-24 21:51:58 -0700

    main header

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -37,5 +37,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
-#endif
 char *_strstr(char *haystack, char *needle);
+#endif
